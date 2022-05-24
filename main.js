2d7ec2cfc728a820ba281b9cfcf30e4c37d45359
sjnt <bragav731@gmail.com>
2022-05-24 11:06:41 +0300

    it shows result searchin in console

--- main.js
+++ main.js
@@ -3,12 +3,24 @@ const initMap = () => {
         center: {latitude: 47.00774402194879, longitude: 28.85061464753502},
         zoom: 8,
     }
+
     const map = new google.maps.Map(document.getElementById("map"), opt)
+
+    // const marker = new google.maps.Marker({
+    //     position: {latitude: 47.00774402194879, longitude: 28.85061464753502},
+    //     map: map,
+    //   });
+
 }
+  
 
 //=============== Victoria's Task ===============\\
 
-const places = []
+const suggestionedPlaces = [
+    {placeName: "Chisinau", lat: 1, long: 2},
+    {placeName: "Dublin", lat: 3, long: 4},
+    {placeName: "Paris", lat: 5, long: 6},
+]
 
 const wrapper = document.createElement("div")
 wrapper.classList.add("wrapper")
@@ -27,14 +39,33 @@ const autocomplete = document.createElement("div")
 autocomplete.classList.add("autocom-box")
 searcher.append(autocomplete)
 
-places.forEach(element => {
-    const list = document.createElement("li")
-    list.textContent = element;
-    autocomplete.append(list);
+const places = document.createElement("ul")
+
+suggestionedPlaces.forEach(element => {
+    const list = document.createElement("li");
+    list.textContent = element.placeName;
+    places.append(list);
 });
+autocomplete.append(places);
 
+const wrapSearch = document.querySelector(".search-input")
+const inputbox = wrapSearch.querySelector("input")
+const suggestions = wrapSearch.querySelector(".autocom-box");
 
+inputbox.onkeyup = (e) => {
+    const usersRequest = e.target.value;
+    if (usersRequest) {
+        const suggested = suggestionedPlaces.filter((request) => {
+            return request.placeName.toLocaleLowerCase().startsWith(usersRequest.toLocaleLowerCase())
+        })
 
+        // const s = suggested.map((request) => {
+        //     return request = '<li>' + request + '</li>';
+        // })
+        // console.log(suggested);
+
+    }
+}
 
 
 //===================== END =====================\\
